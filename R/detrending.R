b8257ecd2de3e836884aec56cb0610e0ddcbcc2e
Sebastian Sippel <ssippel@bgc-jena.mpg.de>
2015-05-29 11:29:00 +0200

    Finally the package is build

--- R/detrending.R
+++ R/detrending.R
@@ -1,6 +1,19 @@
 # Sebastian Sippel, 15.02.2015
 
 # Function to determine trend component with SSA:
+require(spectral.methods)
+
+#' @title A function to estimate nonlinear trends
+#' @export
+#' @description Estimate the nonlinear trend components of a time series using Singular Spectrum Analysis. A wrapper function around filterTSeriesSSA (spectral.methods)
+#' @usage SSA.detrend(data, borders.wl, M, n.comp)
+#' @param data A numeric vector (e.g. a time series), from which the trend component is to be estimated
+#' @param borders.wl See ?filterTSeriesSSA
+#' @param M See ?filterTSeriesSSA
+#' @param n.comp See ?filterTSeriesSSA
+#' @details 
+#' This function calculates the nonlinear trend component. Currently, no padding is implemented, but will come soon.
+#' @return A character vector of length(data) that contains the trend components.
 SSA.detrend <- function(data, borders.wl, M, n.comp) {
   if(any(is.na(data)) | length(which(diff(data) == 0)) > 10) {
     return(rep(NA, length(data))) 
@@ -14,6 +27,14 @@ SSA.detrend <- function(data, borders.wl, M, n.comp) {
 }
 
 # Function to estimate the linear trend components:
+#' @title A function to estimate linear trends
+#' @export
+#' @description Estimates a linear trend.
+#' @usage linear.detrend(data)
+#' @param data A numeric vector (e.g. a time series), from which the trend component is to be estimated
+#' @details 
+#' This function calculates a linear trend component to use for spatio-temporal normalization.
+#' @return A character vector of length(data) that contains the trend components.
 linear.detrend <- function(data) {
   if (any(is.na(data))) {
     return(rep(NA, length(data)))
@@ -21,3 +42,5 @@ linear.detrend <- function(data) {
     return(lm(data ~ c(1:length(data)))$fitted.values)
   }
 }
+
+
--- R/internal.R
+++ R/internal.R
@@ -1,6 +1,8 @@
 # --------------------------------------------------------------------------------
 # Functions to normalize raster data: MAKE THIS INTERNAL FUNCTIONS!
 # --------------------------------------------------------------------------------
+#' @keywords internal
+#' @export
 calculate.mean.grid.cell <- function(x) {
   if (any(is.na(x))) {
     return(NA)
@@ -9,6 +11,9 @@ calculate.mean.grid.cell <- function(x) {
   }
 }
 
+
+#' @keywords internal
+#' @export
 calculate.sd.grid.cell <- function(x) {
   if (any(is.na(x))) {
     return(NA)
